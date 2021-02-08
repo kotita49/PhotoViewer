42bead435dbc8393931cdeff0e7d967cf10253da
kotita49 <kotita49@gmail.com>
2021-02-08 11:46:44 +0000

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { PhotoViewer } from './PhotoViewer/PhotoViewer.js';
+
+function App() {
+  return (
+        
+    <div className="App">
+      <div>
+            <h1>React Photo Viewer</h1>
+            <PhotoViewer/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PhotoViewer/PhotoViewer.js
@@ -0,0 +1,11 @@
+import React from "react";  // import React (to provide access to JSX)
+
+function PhotoViewer() {    // declare a new function called 'PhotoViewer'
+    return (                
+        <div>               // return some JSX 
+            Hello World!
+        </div>
+    );
+}
+
+export {PhotoViewer};       // Export the function so that it can be imported in other files
