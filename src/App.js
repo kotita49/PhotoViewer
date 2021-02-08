7079e3309b86abd8b699e26dd7e19fe35229d043
kotita49 <kotita49@gmail.com>
2021-02-08 15:45:35 +0000

    change image button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import { PhotoViewer } from './PhotoViewer/PhotoViewer.js';
+import { ImageUrls } from './PhotoViewer/PhotoViewer.js';
+import React, { useState } from 'react';
 
 function App() {
+  const [url, setUrl] = useState(ImageUrls[0]);
+
+  function changeImage(){
+  setUrl(ImageUrls[Math.floor(Math.random()*ImageUrls.length)])
+}
+  
   return (
         
     <div className="App">
       <div>
             <h1>React Photo Viewer</h1>
-            <PhotoViewer src={'https://picsum.photos/200/300'}/>
+            <p>The url: {url}</p>
+            <p><img src={url}/></p>
+            <button onClick={ changeImage }>
+        Change Image
+      </button>
         </div>
     </div>
   );
 }
+
 
 export default App;
